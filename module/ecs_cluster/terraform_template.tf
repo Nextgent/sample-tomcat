4f4a230e853f26e816307fa46e487f6c9999483d
Nextgent <45428335+Nextgent@users.noreply.github.com>
2019-06-06 21:27:39 +0530

    Modified by Workflow Agent

--- module/ecs_cluster/terraform_template.tf
+++ module/ecs_cluster/terraform_template.tf
@@ -55,7 +55,7 @@
   "terraform": {
     "backend": {
       "s3": {
-        "bucket": "aws-app-migration-aws",
+        "bucket": "aws-migration-app",
         "key": "ecs/terraform.tfstate",
         "region": "us-east-1"
       }
@@ -103,7 +103,7 @@
       "description": "ECS Task Capabilities"
     },
     "ecs_task_execition_role": {
-      "default": "arn:aws:iam::550229128112:role/ecsTaskExecutionRole",
+      "default": "arn:aws:iam::418476321382:role/ecsTaskExecutionRole",
       "description": "ECS Task Execution Role"
     },
     "fargate_cpu": {
